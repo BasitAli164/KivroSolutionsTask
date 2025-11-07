91988d7b3f36857d4695520eda39018a59cd3282
Basit Ali <opoali911@gmail.com>
2025-11-07 20:06:37 +0500

    install all dependencies

--- Task1/server/index.js
+++ Task1/server/index.js
@@ -3,3 +3,9 @@ import express from 'express'
 
 
 const app=express();
+
+
+const PORT=process.env.PORT || 4000;
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
