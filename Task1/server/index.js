02802a6c734c58d6dd1c670da39ed4dfd4d54ece
Basit Ali <opoali911@gmail.com>
2025-11-07 20:24:42 +0500

    app is almost ready

--- Task1/server/index.js
+++ Task1/server/index.js
@@ -1,8 +1,11 @@
 import express from 'express'
-
-
+import dotenv from 'dotenv'
+import cors from 'cors'
 
 const app=express();
+dotenv.config()
+app.use(cors())
+app.use(express.json())
 app.get('/',(req,res)=>{
     res.json({message:"Talking from server"})
     
