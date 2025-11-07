4c42a9b05dec5bedbcf17382666d20e3fd9136b5
Basit Ali <opoali911@gmail.com>
2025-11-07 20:30:17 +0500

    working in backend of task 1

--- Task1/server/index.js
+++ Task1/server/index.js
@@ -2,8 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 
-const app=express();
 dotenv.config()
+const app=express();
 app.use(cors())
 app.use(express.json())
 app.get('/',(req,res)=>{
