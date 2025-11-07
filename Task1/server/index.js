f228b8a353448814320af9893bf8d34db272cb8a
Basit Ali <opoali911@gmail.com>
2025-11-07 20:20:22 +0500

    install dotenv

--- Task1/server/index.js
+++ Task1/server/index.js
@@ -3,7 +3,10 @@ import express from 'express'
 
 
 const app=express();
-
+app.get('/',(req,res)=>{
+    res.json({message:"Talking from server"})
+    
+})
 
 const PORT=process.env.PORT || 4000;
 app.listen(PORT,()=>{
