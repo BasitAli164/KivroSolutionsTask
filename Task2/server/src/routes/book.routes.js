ec83108036afb077a5b652564870d0a382954887
Basit Ali <opoali911@gmail.com>
2025-11-08 22:31:05 +0500

    complete the routes

---
+++ Task2/server/src/routes/book.routes.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import { addBook, deleteBookById, getAllBook, getBookById } from '../controller/book.controller';
+
+const router=express.Router();
+
+router.post('/addbook',addBook)
+router.get('/getbook/:id',getBookById)
+router.get('/getallbook',getAllBook)
+router.delete('/del/:id',deleteBookById)
+export default router
