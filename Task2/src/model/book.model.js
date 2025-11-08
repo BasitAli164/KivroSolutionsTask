3f07ee47942cde10f16d3b175d20cc3ecaf628a5
Basit Ali <opoali911@gmail.com>
2025-11-08 20:54:45 +0500

    schema already complete

--- Task2/src/model/book.model.js
+++ Task2/src/model/book.model.js
@@ -12,7 +12,19 @@ const bookSchema=new mongoose.Schema({
         required:true,
         trim:true
     },
-    
+    bookNumber:{ // Unique book identifier (International Standard Book Number) 
+        type:String,
+        trim:true
+    },
+    bookType:{  // Type or category of the book (e.g., Fiction, History, Science)
+        type:String,
+        trim:true,
+    },
+    publishYear:{
+        type:Number,
+
+    }
+
 },{
     timestamps:true
 })
