9d030609ba9be08c1d8c598a8419842c2af991ec
Basit Ali <opoali911@gmail.com>
2025-11-08 21:59:19 +0500

    working in controller

--- Task2/server/src/controller/book.controller.js
+++ Task2/server/src/controller/book.controller.js
@@ -1,7 +1,7 @@
 import Book from "../model/book.model";
 
 
-const addBook=async (req,res)=>{
+export const addBook=async (req,res)=>{
     const {title,author,bookUniqeNo,description,publishedYear,totalCopies}=req.body
     console.log("Res.body:",req.body)
     try {
@@ -24,6 +24,53 @@ const addBook=async (req,res)=>{
 
         })
 
+        
+    } catch (error) {
+        console.log("Error during adding book",error)
+        return res.status(500).json({
+            status:500,
+            message:"Server side error.....!",
+            err:error,
+        })
+        
+    }
+}
+
+export const getAllBook=async(req,res)=>{
+    try {
+
+        const allBook=await Book.find()
+        if(!allBook){
+            return res.status(400).json({
+                status:400,
+                message:"Book not found",
+            })
+        } 
+        
+        return res.status(200).json({
+            status:200,
+            message:"All Books Detail are:-",
+            book:allBook
+        })
+    } catch (error) {
+        console.log("Error facing during getting all book:",error)
+        return res.status(500).json({
+            status:500,
+            message:"Server side error",
+            err:error
+        })
+        
+    }
+}
+
+export const getBookById=async(req,res)=>{
+    const {id}=req.params
+    try {
+        const book=await Book.findById(id)
+        console.log("Get book by it id: ",book)
+        if(!book){
+            return res.status(400).json({})
+        }
         
     } catch (error) {
         
