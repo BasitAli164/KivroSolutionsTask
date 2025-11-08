369e880c0e4f008af8ceeed6177587fb080b4dec
Basit Ali <opoali911@gmail.com>
2025-11-08 23:05:10 +0500

    fixing error

--- Task2/server/app.js
+++ Task2/server/app.js
@@ -4,7 +4,8 @@ import router from './src/routes/book.routes.js';
 dotenv.config()
 const app=express()
 
-app.use(express.json({limit:process.env.LIMIT}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true})); 
 app.use('/api/book/',router)
 
 export default app
