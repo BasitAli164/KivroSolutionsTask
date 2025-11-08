61d807a9adfb91a21f5f36f2c4f9e665137035b5
Basit Ali <opoali911@gmail.com>
2025-11-08 21:30:27 +0500

    model is complete

--- Task2/server/src/model/book.model.js
+++ Task2/server/src/model/book.model.js
@@ -1,6 +1,32 @@
 import mongoose from "mongoose";
 
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    author: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    bookUniqeNo: {
+      type: String,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    publishedYear: {
+      type: Number,
+    },
+    totalCopies: {
+      type: Number,
+    },
+  },
+  { timestamps: true }
+);
 
-const bookSchema=new mongoose.Schema({
-    
-},{timestamps:true})
+export default mongoose.model("Book", bookSchema);
