1d404d0308f73fab0495dcec261a56aaf1f31aac
Basit Ali <opoali911@gmail.com>
2025-11-16 20:38:27 +0500

    db connection code and model is ready

---
+++ Task4/server/src/model/User.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const { Schema, model } = mongoose;
+
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export default model("User", UserSchema);
