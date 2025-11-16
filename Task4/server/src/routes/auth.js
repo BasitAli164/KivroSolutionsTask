e5224834f4061922f132228602cb43dfa568167e
Basit Ali <opoali911@gmail.com>
2025-11-16 20:46:10 +0500

    change the port

--- Task4/server/src/routes/auth.js
+++ Task4/server/src/routes/auth.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
-import User from '../models/User.js';
+import User from '../model/User.js';
 import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router();
